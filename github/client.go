1f9503661c08f8fc0845a74d60b8c93b59ed1307
David Schmitz <david.schmitz.privat@gmail.com>
2025-05-23 09:38:35 +0200

    Finished before cleanup

--- github/client.go
+++ github/client.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"log"
+	"os/exec"
 )
 
 type Github interface {
@@ -13,13 +14,13 @@ type GithubClient struct {
 
 func (gh GithubClient) AddSecretToRepository(key string, secret string, repositoy string) (err error) {
 	log.Default().Printf("Adding secret %s", key)
-	// cmd := exec.Command("gh", "secret", "set", key, "--body", secret, "--repo", repositoy)
-	// output, err := cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Default().Printf("Error adding secret %s to repository %s: %v\nOutput: %s",
-	// 		key, repositoy, err, string(output))
-	// 	return
-	// }
+	cmd := exec.Command("gh", "secret", "set", key, "--body", secret, "--repo", repositoy)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Default().Printf("Error adding secret %s to repository %s: %v\nOutput: %s",
+			key, repositoy, err, string(output))
+		return
+	}
 
 	log.Default().Printf("Successfully added secret %s to repository %s", key, repositoy)
 	return nil
