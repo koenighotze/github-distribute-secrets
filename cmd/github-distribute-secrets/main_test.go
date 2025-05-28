374f8be0c6409d2eb974f0acf2d752cd5a8d6966
David Schmitz <david.schmitz.privat@gmail.com>
2025-05-28 17:47:49 +0200

    Add baseline tests

---
+++ cmd/github-distribute-secrets/main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestApplyConfigurationToRepository(t *testing.T) {
+	// TODO
+}
+
+func TestApplyConfiguration(t *testing.T) {
+	// TODO
+}
+
+func TestMain(t *testing.T) {
+	// TODO
+}
--- internal/common/cli/command_runner_test.go
+++ internal/common/cli/command_runner_test.go
@@ -6,45 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMockCommandRunner(t *testing.T) {
-	t.Run("should return expected output for expected commands", func(t *testing.T) {
-		expectedOutput := []byte("test output")
-		mockRunner := &MockCommandRunner{
-			ExpectedCommand: ExpectedCommand{
-				Name:   "echo",
-				Args:   []string{"hello", "world"},
-				Output: expectedOutput,
-				Error:  nil,
-			},
-			T: t,
-		}
-
-		output, err := mockRunner.Run("echo", "hello", "world")
-
-		assert.NoError(t, err)
-		assert.Equal(t, expectedOutput, output)
-	})
-
-	t.Run("should return expected error for commands configured to fail", func(t *testing.T) {
-		expectedError := assert.AnError
-		mockRunner := &MockCommandRunner{
-			ExpectedCommand: ExpectedCommand{
-				Name:   "failing-command",
-				Args:   nil,
-				Output: nil,
-				Error:  expectedError,
-			},
-			T: t,
-		}
-
-		output, err := mockRunner.Run("failing-command")
-
-		assert.Error(t, err)
-		assert.Equal(t, expectedError, err)
-		assert.Empty(t, output)
-	})
-}
-
 func TestNewCommandRunner(t *testing.T) {
 	t.Run("should return a valid command runner", func(t *testing.T) {
 		runner := NewCommandRunner()
@@ -52,5 +13,43 @@ func TestNewCommandRunner(t *testing.T) {
 		assert.NotNil(t, runner)
 		_, ok := runner.(CliCommandRunner)
 		assert.True(t, ok, "Expected NewCommandRunner to return a CliCommandRunner")
+	})
+
+	type mockExec struct {
+		called        bool
+		exectedReturn []byte
+		exectedError  error
+		exec          func(name string, arg ...string) ([]byte, error)
+	}
+
+	defaultMockExec := mockExec{}
+	defaultMockExec.called = false
+	defaultMockExec.exec = func(name string, arg ...string) ([]byte, error) {
+		defaultMockExec.called = true
+		return defaultMockExec.exectedReturn, defaultMockExec.exectedError
+	}
+
+	t.Run("should return the output of the executor", func(t *testing.T) {
+		defaultMockExec.exectedReturn = []byte("thereturn")
+		runner := CliCommandRunner{
+			exec: defaultMockExec.exec,
+		}
+
+		result, err := runner.Run("foo", "bar")
+
+		assert.Equal(t, defaultMockExec.exectedReturn, result)
+		assert.Nil(t, err)
+	})
+	t.Run("should return the error of the executor", func(t *testing.T) {
+		defaultMockExec.exectedReturn = nil
+		defaultMockExec.exectedError = assert.AnError
+		runner := CliCommandRunner{
+			exec: defaultMockExec.exec,
+		}
+
+		result, err := runner.Run("foo", "bar")
+
+		assert.Nil(t, result)
+		assert.Equal(t, assert.AnError, err)
 	})
 }
